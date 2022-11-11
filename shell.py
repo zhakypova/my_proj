3ac9b13867cc002ac50e1fdc8a715b3d432beeda
zhakypova <zulyawayne@gmail.com>
2022-11-11 18:57:02 +0600

    second commit

--- shell.py
+++ shell.py
@@ -5,14 +5,16 @@ from my_app.models import Customer, Account, Work
 with open("data_r.json", "r") as file:
     data = json.load(file)
 
+# print(data)
+
 for i in data:
-    customers=Customer(name=i['name'], phone=i['phone'], email=i['email'])
-    works=Work.objects.create(address=i['address'], city=i['city'], company=i['company'], postalZip=i['postalZip'], customer=customers)
+    customers=Customer.objects.create(name=i['name'], phone=i['phone'], email=i['email'])
+    works=Work.objects.create(address=i['address'], city=i['city'], company=i['company'], postalZip=int(i['postalZip']), customer=customers)
     accounts=Account.objects.create(pin=i['pin'], acc_num=i['acc_num'], pan=i['pan'], cvv=i['cvv'], customer=customers)
 
 
 
-
+print(Customer.objects.all())
 
 
 
